05c595160f1969858faab6ffd3df73319d1aeff3
MrUDDoff <kubasov.den@bk.ru>
2023-06-13 00:18:16 +0700

    fix edit button and github from nav

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,12 +33,12 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/mruddoff/UsefulInfo/tree/master',
         },
         // blog: {
         //   showReadingTime: true,
         //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //     'https://github.com/mruddoff/UsefulInfo/tree/master',
         // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -76,7 +76,7 @@ const config = {
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/mruddoff/UsefulInfo',
             label: 'GitHub',
             position: 'right',
           },
