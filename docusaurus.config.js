bd178f5a9fb1167cdf65fc504daed26c3bce95cd
MrUDDoff <kubasov.den@bk.ru>
2023-06-12 23:54:49 +0700

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,95 @@
+// @ts-check
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Useful Info',
+  tagline: 'Полезные команды и гайды.',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://mruddoff.github.io',
+  baseUrl: '/UsefulInfo',
+
+  organizationName: 'mruddoff',
+  projectName: 'UsefulInfo',
+  deploymentBranch: 'gh-pages',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      navbar: {
+        title: 'UsefulInfo',
+        logo: {
+          alt: 'Useful Logo',
+          src: 'img/favicon.ico',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'base',
+            position: 'left',
+            label: 'Основное',
+          },
+          {
+            type: 'docSidebar',
+            position: 'left',
+            sidebarId: 'linux',
+            label: 'Linux',
+          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Built with Docusaurus. Useful Info ${new Date().getFullYear()} • Права проекта под защитой.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+];
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+      </div>
+    </section>
+  );
+}
